59ed69139c65306ac222b41adfefcd7e3225ffa0
Bawazier <32612909+Bawazier@users.noreply.github.com>
2020-11-20 19:39:31 +0700

    fix header navigation

--- App/navigations/index.js
+++ App/navigations/index.js
@@ -8,6 +8,20 @@ import Settings from '../screens/Settings';
 import Contacts from '../screens/Contacts';
 import Profile from '../screens/Profile';
 
+import {
+  Button,
+  Text,
+  Icon,
+  List,
+  ListItem,
+  Left,
+  Body,
+  Picker,
+  Thumbnail,
+} from 'native-base';
+
+import SideBar from '../components/SideBar';
+
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {createDrawerNavigator} from '@react-navigation/drawer';
@@ -23,28 +37,158 @@ const AuthStack = () => {
         component={Main}
         options={{headerShown: false}}
       />
-      <Stack.Screen name="Login" component={Login} />
-      <Stack.Screen name="Profile" component={Profile} />
+      <Stack.Screen
+        name="Login"
+        component={Login}
+        options={{
+          title: '',
+          cardShadowEnabled: false,
+          headerTintColor: '#2f4562',
+          headerTransparent: true,
+        }}
+      />
+      <Stack.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          title: '',
+          cardShadowEnabled: false,
+          headerStyle: {backgroundColor: '#152642', height: 50},
+          headerLeft: () => <Text>&nbsp;</Text>,
+          headerRight: () => (
+            <Button info style={{color: '#fff', width: 150, height: 50}}>
+              <Text>START</Text>
+              <Icon name="long-arrow-right" type="FontAwesome" />
+            </Button>
+          ),
+        }}
+      />
     </Stack.Navigator>
   );
 };
 
 const ChatStack = () => {
   return (
-    <Stack.Navigator
-      sceneContainerStyle={{backgroundColor: '#2E4392'}}
-      headerMode="screen">
-      <Stack.Screen name="ChatList" component={ChatList} />
-      <Stack.Screen name="ChatRoom" component={ChatRoom} />
+    <Stack.Navigator>
+      <Stack.Screen
+        name="ChatList"
+        component={ChatList}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="ChatRoom"
+        component={ChatRoom}
+        options={{
+          headerStyle: {backgroundColor: '#152642'},
+          cardShadowEnabled: false,
+          headerTintColor: '#2f4562',
+          headerRightContainerStyle: {marginHorizontal: 25},
+          headerTitleStyle: {marginHorizontal: -10},
+          headerTitleContainerStyle: {marginLeft: -35},
+          headerTitle: () => (
+            <List style={{}}>
+              <ListItem avatar>
+                <Left>
+                  <Thumbnail
+                    source={{
+                      uri:
+                        'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.slashfilm.com%2Fwp%2Fwp-content%2Fimages%2Favatar2-jake-navi-screaming.jpg&f=1&nofb=1',
+                    }}
+                    style={{width: 40, height: 40}}
+                  />
+                </Left>
+                <Body style={{borderBottomWidth: 0}}>
+                  <Text style={{color: '#e6e9ef'}}>Kumar Pratik</Text>
+                  <Text note style={{color: '#2f4562'}}>
+                    last seen 12.11.20
+                  </Text>
+                </Body>
+              </ListItem>
+            </List>
+          ),
+          headerRight: () => (
+            <Icon
+              name="ellipsis-v"
+              type="FontAwesome"
+              style={{color: '#2f4562', fontSize: 25}}
+            />
+          ),
+        }}
+      />
     </Stack.Navigator>
   );
 };
 
-const SettingStack = () => {
+const SettingStack = ({navigation}) => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Settings" component={Settings} />
-      <Stack.Screen name="Profile" component={Profile} />
+      <Stack.Screen
+        name="Settings"
+        component={Settings}
+        options={{
+          headerStyle: {backgroundColor: '#152642'},
+          cardShadowEnabled: false,
+          headerTintColor: '#2f4562',
+          headerRightContainerStyle: {marginHorizontal: 25},
+          headerLeftContainerStyle: {marginHorizontal: 20},
+          headerTransparent: true,
+          headerLeft: () => (
+            <Icon
+              name="long-arrow-left"
+              type="FontAwesome"
+              style={{color: '#2f4562'}}
+              onPress={() => navigation.goBack()}
+            />
+          ),
+          headerTitle: () => (
+            <Text style={{color: '#fff', fontSize: 24}}>Settings</Text>
+          ),
+          headerRight: () => (
+            <Icon
+              name="ellipsis-v"
+              type="FontAwesome"
+              style={{color: '#2f4562', fontSize: 25}}
+            />
+          ),
+        }}
+      />
+      <Stack.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          headerStyle: {backgroundColor: '#152642'},
+          cardShadowEnabled: false,
+          headerTintColor: '#2f4562',
+          headerTitle: () => <Text style={{color: '#fff'}}>Info</Text>,
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const ContactStack = ({navigation}) => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Contacts"
+        component={Contacts}
+        options={{
+          title: '',
+          headerTransparent: true,
+          headerLeftContainerStyle: {marginHorizontal: 20},
+          headerLeft: () => (
+            <Icon
+              name="long-arrow-left"
+              type="FontAwesome"
+              style={{color: '#2f4562'}}
+              onPress={() => navigation.goBack()}
+            />
+          ),
+          headerTitle: () => (
+            <Text style={{color: '#fff', fontSize: 24}}>Contacts</Text>
+          ),
+        }}
+      />
     </Stack.Navigator>
   );
 };
@@ -52,18 +196,12 @@ const SettingStack = () => {
 const Navigations = () => {
   return (
     <NavigationContainer>
-      {true ? (
+      {false ? (
         <AuthStack />
       ) : (
-        <Drawer.Navigator
-          initialRouteName="Home"
-          sceneContainerStyle={{backgroundColor: '#E6E9EF'}}>
-          <Drawer.Screen
-            name="Home"
-            component={ChatStack}
-            headerMode="screen"
-          />
-          <Drawer.Screen name="Contacts" component={Contacts} />
+        <Drawer.Navigator initialRouteName="Home" drawerContent={SideBar}>
+          <Drawer.Screen name="Home" component={ChatStack} />
+          <Drawer.Screen name="Contacts" component={ContactStack} />
           <Drawer.Screen name="Settings" component={SettingStack} />
         </Drawer.Navigator>
       )}
