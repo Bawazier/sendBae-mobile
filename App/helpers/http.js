df620d9fb973ebd5c8d8caa96c25f425cd1a3bd6
Bawazier <32612909+Bawazier@users.noreply.github.com>
2020-11-24 07:32:48 +0700

    create http helpers

---
+++ App/helpers/http.js
@@ -0,0 +1,13 @@
+import {default as axios} from 'axios';
+
+import {API_URL} from '@env';
+
+export default (token = false) => {
+  console.log(API_URL);
+  return axios.create({
+    baseURL: API_URL,
+    headers: {
+      Authorization: token ? `Bearer ${token}` : undefined,
+    },
+  });
+};
