9e7d1a0a1dcf11a2ede28b0beb26bc30c7f2c348
Bawazier <32612909+Bawazier@users.noreply.github.com>
2020-11-24 07:34:27 +0700

    managing data for login

--- App/screens/Login.js
+++ App/screens/Login.js
@@ -1,73 +1,169 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {Formik} from 'formik';
+import * as Yup from 'yup';
 import {View, Text, Button, Item, Input, Form} from 'native-base';
 
 import {useNavigation} from '@react-navigation/native';
 
+//Components
+import CountryDialog from '../components/CountryDialog';
+
+//Actions
+import AuthActions from '../redux/actions/auth';
+import CountryActions from '../redux/actions/country';
+
 const Login = () => {
+  const [selectCountry, setSelectCountry] = useState(false);
+  const [countryName, setCountryName] = useState('Indonesia');
+  const [countryCode, setCountryCode] = useState('+62');
+  const country = useSelector((state) => state.country);
+  const dispatch = useDispatch();
   const navigation = useNavigation();
+
+  const validationSchema = Yup.object({
+    phoneNumber: Yup.number()
+      .integer('Invalid phone number. Please try again.')
+      .min(1000000000, 'Invalid phone number. Please try again.')
+      .max(99999999999, 'Invalid phone number. Please try again.'),
+  });
+
+  const onSelectCountry = async () => {
+    await dispatch(CountryActions.getCountry());
+    setSelectCountry(true);
+  };
+
+  const handleCancel = async () => {
+    await setSelectCountry(!selectCountry);
+  };
+
+  const handleSelect = async (id) => {
+    await dispatch(CountryActions.getCountryId(id));
+    setSelectCountry(!selectCountry);
+    setCountryName(country.dataId.name);
+    setCountryCode(country.dataId.code);
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#152642',
+    <Formik
+      initialValues={{
+        countryName: countryName,
+        countryCode: countryCode,
+        phoneNumber: '',
+      }}
+      validationSchema={validationSchema}
+      onSubmit={(values) => {
+        const data = {
+          countryId: country.dataId.id || 1,
+          phoneNumber: values.phoneNumber,
+        };
+        dispatch(AuthActions.login(data));
+        // navigation.navigate('Profile');
       }}>
-      <View
-        style={{
-          alignItems: 'flex-start',
-          justifyContent: 'center',
-          margin: 40,
-        }}>
-        <Text style={{color: '#e6e9ef', fontSize: 25, marginVertical: 10}}>
-          Your Phone Number
-        </Text>
-        <Text style={{color: '#2f4562', fontSize: 15}}>
-          Please confirm your country code and enter your mobile phone number
-        </Text>
-      </View>
-      <View
-        style={{
-          alignItems: 'flex-start',
-          justifyContent: 'center',
-          margin: 40,
-        }}>
-        <Form>
-          <Item style={{width: '100%', borderColor: '#2f4562'}}>
-            <Input
-              value="Indonesia"
-              style={{textAlign: 'left', color: '#e6e9ef'}}
-            />
-          </Item>
+      {({
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+        touched,
+        values,
+        errors,
+      }) => (
+        <View
+          style={{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundColor: '#152642',
+          }}>
           <View
             style={{
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-              marginLeft: 15,
+              alignItems: 'flex-start',
+              justifyContent: 'center',
+              margin: 40,
             }}>
-            <Item style={{width: '20%', borderColor: '#2f4562'}}>
-              <Input
-                value="+62"
-                disabled
-                style={{textAlign: 'center', color: '#e6e9ef'}}
-              />
-            </Item>
-            <Item style={{width: '75%', borderColor: '#4995be'}}>
-              <Input style={{color: '#e6e9ef'}} />
-            </Item>
+            <Text style={{color: '#e6e9ef', fontSize: 25, marginVertical: 10}}>
+              Your Phone Number
+            </Text>
+            <Text style={{color: '#2f4562', fontSize: 15}}>
+              Please confirm your country code and enter your mobile phone
+              number
+            </Text>
           </View>
-        </Form>
-      </View>
-      <View style={{width: '100%', paddingHorizontal: 35}}>
-        <Button
-          full
-          style={{backgroundColor: '#4995be', color: '#e6e9ef'}}
-          onPress={() => navigation.navigate('Profile')}>
-          <Text>NEXT</Text>
-        </Button>
-      </View>
-    </View>
+          <View
+            style={{
+              alignItems: 'flex-start',
+              justifyContent: 'center',
+              margin: 40,
+            }}>
+            <Form>
+              <Item
+                style={{width: '100%', borderColor: '#2f4562'}}
+                onPress={onSelectCountry}>
+                <Input
+                  name="countryName"
+                  value={countryName}
+                  disabled
+                  style={{textAlign: 'left', color: '#e6e9ef'}}
+                />
+              </Item>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+                  alignItems: 'center',
+                  marginLeft: 15,
+                }}>
+                <Item style={{width: '20%', borderColor: '#2f4562'}}>
+                  <Input
+                    name="countryCode"
+                    value={countryCode}
+                    disabled
+                    style={{textAlign: 'center', color: '#e6e9ef'}}
+                  />
+                </Item>
+                <Item
+                  style={{
+                    width: '75%',
+                    borderColor: errors.phoneNumber ? '#F01F0E' : '#4995be',
+                  }}>
+                  <Input
+                    name="phoneNumber"
+                    keyboardType="phone-pad"
+                    onChangeText={handleChange('phoneNumber')}
+                    onBlur={handleBlur('phoneNumber')}
+                    value={values.phoneNumber}
+                    autoFocus
+                    style={{color: '#e6e9ef'}}
+                  />
+                </Item>
+              </View>
+              {!touched.phoneNumber && errors.phoneNumber && (
+                <Text
+                  style={{color: '#2f4562', fontSize: 15, textAlign: 'center'}}>
+                  {errors.phoneNumber}
+                </Text>
+              )}
+            </Form>
+          </View>
+          <View style={{width: '100%', paddingHorizontal: 35}}>
+            <Button
+              full
+              style={{backgroundColor: '#4995be', color: '#e6e9ef'}}
+              onPress={handleSubmit}
+              title="Submit"
+              {...(isSubmitting ? 'disabled' : null)}>
+              <Text>NEXT</Text>
+            </Button>
+          </View>
+          <CountryDialog
+            visible={selectCountry}
+            handleCancel={handleCancel}
+            handleSelect={handleSelect}
+          />
+        </View>
+      )}
+    </Formik>
   );
 };
 
