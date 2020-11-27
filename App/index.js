4c90722fc3df3da1d5eccb7b783167a88b059d30
Bawazier <32612909+Bawazier@users.noreply.github.com>
2020-11-27 12:46:14 +0700

    set splash screen app

--- App/index.js
+++ App/index.js
@@ -9,6 +9,7 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 import store from './redux/store';
+import SplashScreen from 'react-native-splash-screen';
 import {persistStore} from 'redux-persist';
 import {PersistGate} from 'redux-persist/es/integration/react';
 
@@ -16,17 +17,24 @@ import {MainContainer} from './styles';
 
 import Screens from './navigations';
 
-const App = () => {
-  const persitedStore = persistStore(store);
-  return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persitedStore}>
-        <MainContainer>
-          <Screens />
-        </MainContainer>
-      </PersistGate>
-    </Provider>
-  );
-};
+class App extends React.Component {
+  componentDidMount() {
+    // do stuff while splash screen is shown
+    // After having done stuff (such as async tasks) hide the splash screen
+    SplashScreen.hide();
+  }
+  render() {
+    const persitedStore = persistStore(store);
+    return (
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persitedStore}>
+          <MainContainer>
+            <Screens />
+          </MainContainer>
+        </PersistGate>
+      </Provider>
+    );
+  }
+}
 
 export default App;
