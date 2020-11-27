79c3709b4c84d60047d4d9b5f3ec6d7b7516d952
Bawazier <32612909+Bawazier@users.noreply.github.com>
2020-11-27 13:56:00 +0700

    set splash screen app

--- android/app/src/main/java/com/sendbae/MainActivity.java
+++ android/app/src/main/java/com/sendbae/MainActivity.java
@@ -2,10 +2,9 @@ package com.sendbae;
 
 import android.os.Bundle; // here 
 import com.facebook.react.ReactActivity;
+
 // react-native-splash-screen >= 0.3.1 
 import org.devio.rn.splashscreen.SplashScreen; // here 
-// react-native-splash-screen < 0.3.1 
-import com.cboy.rn.splashscreen.SplashScreen; // here 
 
 public class MainActivity extends ReactActivity {
   @Override
